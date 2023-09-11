410e8cc5c6915ad917816ccfa3e641f29420b5c9
Chrom Ansil <98952763+Chromansil@users.noreply.github.com>
2023-09-11 12:34:06 +0300

    Update mass_svod.py

--- mass_svod.py
+++ mass_svod.py
@@ -10,25 +10,52 @@ def str_to_list (line):
 
 # Определение функции для перевода списка (list) в строку (str)
 def list_to_str (data):
-    line = ''
-    for i in range(14):
-        line = line + f'{float(data[i]):.2f}' + ('\t')
+    line = str(data[0])
+    for i in range(13):
+        line = line + ('\t') + f'{float(data[i + 1]):.2f}'
     return line
 
 # Работа с txt файлом исходных данных
 d = open('Data.txt', 'r+')
 d.readline()
-ukaz = d.tell()
-line_1 = d.readline()
-data_1 = str_to_list(line_1)
+
+# ukaz = d.tell()
+# line_1 = d.readline()
+# data_1 = str_to_list(line_1)
+# print(data_1)
 
 k0 = 40.00
-k = 192.00
+k = 384.00
 n = 238.00
 
-data_1[8], data_1[9], data_1[10] = f'{k0:.2f}', f'{k0*9.8*2:.2f}', f'{0:.2f}'
-line_1 = list_to_str(data_1)
-d.seek(ukaz)
-d.write(line_1)
+# data_1[8], data_1[9], data_1[10] = f'{k0:.2f}', f'{k0*9.8*2:.2f}', f'{0:.2f}'
+# line_1 = list_to_str(data_1)
+# print(line_1)
+# d.seek(ukaz)
+# d.write(line_1)
+
+# for i in range(3):
+#     ukaz = d.tell()
+#     print(ukaz)
+#     line_1 = d.readline()
+
+for i in range(3):
+    ukaz = d.tell()
+    print(ukaz)
+    line_1 = d.readline()
+    # print(line_1)
+    data_1 = str_to_list(line_1)
+    print(i, ': ', data_1)
+    if i == 0:
+        data_1[8], data_1[9], data_1[10] = f'{k0:.2f}', f'{k0*9.8*2:.2f}', f'{0:.2f}'
+    else:
+        data_1[8], data_1[9], data_1[10] = f'{k0:.2f}', f'{k:.2f}', f'{n:.2f}'
+    line_1 = list_to_str(data_1)
+    print('str', line_1)
+    print(d.tell())
+    d.seek(ukaz)
+    d.write(line_1 + '\n')
+    # d.write(line_1)
+    # d.readline()
 
 d.close()
